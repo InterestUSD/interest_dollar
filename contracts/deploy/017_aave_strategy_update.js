0fbfc2c3f8969693975aae48aea3e2f9744bbb2f
Ashutosh Varma <ashutoshvarma11@live.com>
2021-07-02 14:34:50 +0530

    feat: upgrade aaveStrategy for MOO staking in alfajores

--- contracts/deploy/017_aave_strategy_update.js
+++ contracts/deploy/017_aave_strategy_update.js
@@ -4,6 +4,8 @@ const {
   isAlfajores,
   isSmokeTest,
   getAssetAddresses,
+  isMainnetOrFork,
+  isMainnetOrAlfajoresOrFork,
 } = require("../test/helpers.js");
 const addresses = require("../utils/addresses");
 const {
@@ -20,6 +22,7 @@ const deployName = "017_aave_strategy_update";
 
 const runDeployment = async (hre) => {
   console.log(`Running ${deployName} deployment...`);
+  const assetAddresses = await getAssetAddresses(hre.deployments);
   const { governorAddr } = await hre.getNamedAccounts();
 
   // Signers
@@ -29,59 +32,53 @@ const runDeployment = async (hre) => {
 
   // Deploy Updated Aave strategy.
   const dAaveStrategy = await deployWithConfirmation("AaveStrategy");
-
   const aaveStrategy = await ethers.getContract("AaveStrategy");
 
-
-  const assetAddr = await getAssetAddresses(hre.deployments);
-  // Configure new aave strategy for MOO staking
-  if (isMainnet) {
-    // set uniswap router
-    await aaveStrategy
-      .connect(sGovernor)
-      .setUniswapAddress(addresses.mainnet.uniswapRouter);
-    // set the LP pair, mcUSD(cUSD) - mcEUR(cEUR)
-    await aaveStrategy
-      .connect(sGovernor)
-      .setRewardLpPair(assetAddr.CUSD, assetAddr.CEUR);
-  } else if (isAlfajores) {
-    // set uniswap router
-    log("Setting Uniswap router address in new Aave strategy")
-    await aaveStrategy
-      .connect(sGovernor)
-      .setUniswapAddress(addresses.alfajores.uniswapRouter);
-    // set the LP pair, mcUSD(cUSD) - mcEUR(cEUR)
-    log("Setting mcUSD-mcEUR LP Pair in new Aave strategy")
-    await aaveStrategy
-      .connect(sGovernor)
-      .setRewardLpPair(assetAddr.CUSD, assetAddr.CEUR);
-  } else {
-    // set uniswap router
-    await aaveStrategy
-      .connect(sGovernor)
-      .setUniswapAddress(
-        (await ethers.getContract("MockUniswapRouter")).address
-      );
-    // set the LP pair, mcUSD(cUSD) - mcEUR(cEUR)
-    await aaveStrategy
-      .connect(sGovernor)
-      .setRewardLpPair(assetAddr.CUSD, assetAddr.CEUR);
-    // set the staking contract address
-    await aaveStrategy
-      .connect(sGovernor)
-      .setStakingAddress((await ethers.getContract("MockMOOStaking")).address);
-  }
-
   // Proposal for the governor update the contract
-  const propDescription = "Update Aave implementation";
+  const propDescription =
+    "Update Aave implementation, enable cEUR and set MOO as reward token";
   const propArgs = await proposeArgs([
+    // upgrade AaveStrategy
     {
       contract: aaveStrategyProxy,
       signature: "upgradeTo(address)",
       args: [dAaveStrategy.address],
     },
+    // set MOO as reward token
+    {
+      contract: aaveStrategy,
+      signature: "setRewardTokenAddress(address)",
+      args: [assetAddresses.MOO],
+    },
+    // enable cEUR deposits
+    {
+      contract: aaveStrategy,
+      signature: "setPTokenAddress(address,address)",
+      args: [assetAddresses.CEUR, assetAddresses.mCEUR],
+    },
+    // set uniswap router address in new implementation
+    {
+      contract: aaveStrategy,
+      signature: "setUniswapAddress(address)",
+      args: [
+        isMainnetOrFork
+          ? addresses.mainnet.uniswapRouter
+          : addresses.alfajores.uniswapRouter,
+      ],
+    },
+    // set staking contract address in new implementation
+    {
+      contract: aaveStrategy,
+      signature: "setStakingAddress(address)",
+      args: [
+        isMainnetOrFork
+          ? addresses.zero
+          : (await ethers.getContract("MockMOOStaking")).address,
+      ],
+    },
   ]);
 
+  log(`Proposal: ${propDescription}`);
   if (isMainnet) {
     // On Mainnet, only propose. The enqueue and execution are handled manually via multi-sig.
     log("Sending proposal to governor...");
@@ -94,11 +91,9 @@ const runDeployment = async (hre) => {
     log("Proposal executed.");
   } else {
     await withConfirmation(
-      aaveStrategyProxy
-        .connect(sGovernor)
-        .upgradeTo(dAaveStrategy.address)
+      aaveStrategyProxy.connect(sGovernor).upgradeTo(dAaveStrategy.address)
     );
-    log("Switched implementation of AaveStrategy");
+    log(`Successfully Executed: ${propDescription}`);
   }
 
   return true;
