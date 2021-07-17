d9504672bdfe29b2afab7035a7db80eddebdfd23
Ashutosh Varma <ashutoshvarma11@live.com>
2021-07-17 16:35:55 +0530

    feat: make `MockMintableUniswapPair` burnable

--- contracts/contracts/mocks/MockMintableUniswapPair.sol
+++ contracts/contracts/mocks/MockMintableUniswapPair.sol
@@ -2,8 +2,13 @@ pragma solidity 0.5.11;
 
 import "./MintableERC20.sol";
 import "./MockUniswapPair.sol";
+import { ERC20Burnable } from "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 
-contract MockMintableUniswapPair is MockUniswapPair, MintableERC20 {
+contract MockMintableUniswapPair is
+    MockUniswapPair,
+    MintableERC20,
+    ERC20Burnable
+{
     uint256 public constant decimals = 18;
     string public constant symbol = "Uniswap V2";
     string public constant name = "UNI-V2";
