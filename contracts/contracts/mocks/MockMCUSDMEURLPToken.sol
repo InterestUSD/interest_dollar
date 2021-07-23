36c35eef4fb2700b40645fce0c6ea9b3e0075450
Ashutosh Varma <ashutoshvarma11@live.com>
2021-07-23 14:38:24 +0530

    fix: change mock LP Token to minatable uni lp token

--- contracts/contracts/mocks/MockMCUSDMEURLPToken.sol
+++ contracts/contracts/mocks/MockMCUSDMEURLPToken.sol
@@ -1,8 +1,8 @@
 pragma solidity 0.5.11;
 
-import "./MintableERC20.sol";
+import "./MockMintableUniswapPair.sol";
 
-contract MockMCUSDMEURLPToken is MintableERC20 {
+contract MockMCUSDMEURLPToken is MockMintableUniswapPair {
     uint256 public constant decimals = 18;
     string public constant symbol = "UNI-MCUSD-MCEUR";
     string public constant name = "Uni LP Token mcUSD-mcEUR";
