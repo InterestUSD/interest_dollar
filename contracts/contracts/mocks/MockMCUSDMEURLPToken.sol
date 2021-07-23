df99b4124a9bd89983cd8fdc4c5a35184874abf4
Ashutosh Varma <ashutoshvarma11@live.com>
2021-07-23 22:47:54 +0530

    fix: add constructor in mock lp token

--- contracts/contracts/mocks/MockMCUSDMEURLPToken.sol
+++ contracts/contracts/mocks/MockMCUSDMEURLPToken.sol
@@ -6,4 +6,11 @@ contract MockMCUSDMEURLPToken is MockMintableUniswapPair {
     uint256 public constant decimals = 18;
     string public constant symbol = "UNI-MCUSD-MCEUR";
     string public constant name = "Uni LP Token mcUSD-mcEUR";
+
+    constructor(
+        address _token0,
+        address _token1,
+        uint112 _reserve0,
+        uint112 _reserve1
+    ) public MockMintableUniswapPair(_token0, _token1, _reserve0, _reserve1) {}
 }
