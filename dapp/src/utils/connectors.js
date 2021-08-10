778f296ee46ff2d7e649fbd86b7a3695e4b7f9de
Deepanshu Hooda <hoodaisdeepanshu@gmail.com>
2021-08-10 20:14:46 +0530

    feat(dapp): add support for celo extension wallet

--- dapp/src/utils/connectors.js
+++ dapp/src/utils/connectors.js
@@ -1,64 +1,42 @@
 import { InjectedConnector } from '@web3-react/injected-connector'
-import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { LedgerConnector } from './LedgerConnector'
-import { MewConnectConnector } from '@myetherwallet/mewconnect-connector'
+import { InjectedConnector as CEWConnector } from '@ubeswap/injected-connector'
 
 import { providerName } from 'utils/web3'
 
 const POLLING_INTERVAL = 12000
 const RPC_HTTP_URLS = {
-  1: process.env.RPC_HTTP_URL_1,
-  4: process.env.RPC_HTTP_URL_4,
-}
-const RPC_WS_URLS = {
-  1: process.env.RPC_WS_URL_1,
-  4: process.env.RPC_WS_URL_4,
+  42220: process.env.RPC_HTTP_URL_42220,
+  44787: process.env.RPC_HTTP_URL_44787,
 }
 
 const getChainId = () => {
   if (process.env.NODE_ENV === 'production') {
-    return 1
+    return 42220
   } else if (process.env.NODE_ENV === 'development') {
-    return process.env.MAINNET_FORK ? 1 : 31337
+    return process.env.MAINNET_FORK ? 42220 : 31337
   }
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42, 31337],
+  supportedChainIds: [42220, 44787, 31337],
+})
+
+export const celoExtensionWallet = new CEWConnector({
+  supportedChainIds: [42220, 44787, 31337],
 })
 
 export const ledger = new LedgerConnector({
   chainId: getChainId(),
-  url: RPC_HTTP_URLS[1],
+  url: RPC_HTTP_URLS[getChainId()],
   pollingInterval: POLLING_INTERVAL,
-})
-
-export const mewConnect = new MewConnectConnector({
-  url: RPC_WS_URLS[1],
-})
-
-export const walletConnect = new WalletConnectConnector({
-  rpc: {
-    // Note: WalletConnect Connector doesn't work
-    // with networks other than mainnet
-    1: RPC_HTTP_URLS[1],
-  },
-  pollingInterval: POLLING_INTERVAL,
-})
-
-// Clear WalletConnect's state on disconnect
-walletConnect.on('disconnect', () => {
-  console.log('Cleaning up...')
-  delete localStorage.walletconnect
 })
 
 export const getConnectorImage = (activeConnector) => {
   if (activeConnector.connector === ledger) {
     return 'ledger-icon.svg'
-  } else if (activeConnector.connector === mewConnect) {
-    return 'mew-icon.svg'
-  } else if (activeConnector.connector === walletConnect) {
-    return 'walletconnect-icon.svg'
+  } else if (activeConnector.connector === celoExtensionWallet) {
+    return 'metamask-icon.svg'
   } else {
     const prName = providerName()
     if (prName === 'metamask') {
@@ -81,19 +59,14 @@ export const connectorsByName = {
     displayName: 'MetaMask',
     fileName: 'metamask',
   },
+  CeloExtensionWallet: {
+    connector: celoExtensionWallet,
+    displayName: 'CeloExtensionWallet',
+    fileName: 'celoextensionwallet',
+  },
   Ledger: {
     connector: ledger,
     displayName: 'Ledger',
     fileName: 'ledger',
-  },
-  MEW: {
-    connector: mewConnect,
-    displayName: 'MEW wallet',
-    fileName: 'mew',
-  },
-  WalletConnect: {
-    connector: walletConnect,
-    displayName: 'WalletConnect',
-    fileName: 'walletconnect',
   },
 }
